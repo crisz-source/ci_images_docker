a2a255bbb9ec55ab049af335227b076d5ba1bcdf
crisz-source <crishubgit@hotmail.com>
2024-12-16 10:24:20 -0300

    agora vai

--- test_increment.sh
+++ test_increment.sh
@@ -2,7 +2,6 @@
 
 # Nome do arquivo de versão
 VERSION_FILE="version.txt"
-LOG_FILE="logs_image.txt"
 
 # Checar se o arquivo de versão existe
 if [[ ! -f "$VERSION_FILE" ]]; then
@@ -22,11 +21,3 @@ echo "$NEW_VERSION" > "$VERSION_FILE"
 # Exibir as versões (apenas para debug, opcional)
 echo "Versão anterior: $CURRENT_VERSION"
 echo "Nova versão: $NEW_VERSION"
-
-# Salvar um log 
-echo "Versão das imagens alterada de $CURRENT_VERSION para $NEW_VERSION" > $LOG_FILE
-
-# Agora, vamos garantir que os arquivos alterados sejam commitados e enviados ao GitHub
-git add $VERSION_FILE $LOG_FILE
-git commit -m "Atualiza as imagens e a versão para $NEW_VERSION"
-git push origin main
